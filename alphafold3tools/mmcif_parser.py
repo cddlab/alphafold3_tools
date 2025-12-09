ab61f3422aecf30e9ce7133118685a873ef67ec3
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-12-10 01:10:16 +0900

    add sections without atom_sites

--- alphafold3tools/mmcif_parser.py
+++ alphafold3tools/mmcif_parser.py
@@ -1,19 +1,7 @@
 # %%
-import dataclasses
-import datetime
-import functools
-import json
-import os
-import re
-import shutil
-import subprocess
-import sys
-from collections.abc import Iterable, Iterator, Mapping, Sequence
 from pathlib import Path
-from typing import IO, Any, Final, Self, TypeAlias
 
 import gemmi
-import numpy as np
 import pandas as pd
 from loguru import logger
 
@@ -36,16 +24,17 @@ def get_entity_id_for_chain(df_entity_polys, target_chain: str) -> str | None:
     return matching_rows.iloc[0]["entity_id"]
 
 
-ciffile = "/Users/YoshitakaM/Desktop/mmcif_files/4HHB.cif"
-outcif = "/Users/YoshitakaM/Desktop/mmcif_files/4HHB_out.cif"
+pdbid = "4HHB"
 target_chain = "A"
+
+ciffile = f"/Users/YoshitakaM/Desktop/mmcif_files/{pdbid}.cif"
 doc = gemmi.cif.read_file(ciffile)
 block = doc.sole_block()
 names = block.get_mmcif_category_names()
-# _chem_compを抜き出し
+# _chem_comp
 chem_comps = block.get_mmcif_category("_chem_comp")
 sorted_chem_comps = _sort_dict_by_keys(chem_comps)
-# _entityを抜き出し
+# _entity
 entities = block.get_mmcif_category("_entity")
 entity_polys = block.get_mmcif_category("_entity_poly")
 entity_poly_seqs = block.get_mmcif_category("_entity_poly_seq")
@@ -68,8 +57,8 @@ df_entity_poly_seqs = pd.DataFrame(entity_poly_seqs)
 target_poly_seqs = df_entity_poly_seqs.loc[
     df_entity_poly_seqs["entity_id"] == target_entity_id
 ]
-# always set hetero to "n"
-target_poly_seqs.loc[:, "hetero"] = "n"
+
+target_poly_seqs.loc[:, "hetero"] = "n"  # always set hetero to "n"
 polyseq_loop_dict = {
     "entity_id": target_poly_seqs["entity_id"].to_list(),
     "hetero": target_poly_seqs["hetero"].to_list(),
@@ -96,8 +85,7 @@ df_pdbx_poly_seq_scheme = pd.DataFrame(pdbx_poly_seq_scheme)
 target_pdbx_poly_seq_scheme = df_pdbx_poly_seq_scheme.loc[
     df_pdbx_poly_seq_scheme["entity_id"] == target_entity_id
 ].loc[df_pdbx_poly_seq_scheme["pdb_strand_id"] == target_chain]
-# always set hetero to "n"
-target_pdbx_poly_seq_scheme.loc[:, "hetero"] = "n"
+target_pdbx_poly_seq_scheme.loc[:, "hetero"] = "n"  # always set hetero to "n"
 pdbx_polyseq_loop_dict = {
     "asym_id": target_pdbx_poly_seq_scheme["asym_id"].to_list(),
     "auth_seq_num": target_pdbx_poly_seq_scheme["auth_seq_num"].to_list(),
@@ -109,8 +97,51 @@ pdbx_polyseq_loop_dict = {
     "pdb_strand_id": target_pdbx_poly_seq_scheme["pdb_strand_id"].to_list(),
     "seq_id": target_pdbx_poly_seq_scheme["seq_id"].to_list(),
 }
+# _pdbx_struct_assembly
+pdbx_struct_assembly = block.get_mmcif_category("_pdbx_struct_assembly")
+df_pdbx_struct_assembly = pd.DataFrame(pdbx_struct_assembly)
+target_pdbx_struct_assembly = df_pdbx_struct_assembly.loc[
+    df_pdbx_struct_assembly["id"] == "1"
+]
+pdbx_struct_assembly_dict = {
+    "details": target_pdbx_struct_assembly["details"].values[0],
+    "id": target_pdbx_struct_assembly["id"].values[0],
+    "method_details": target_pdbx_struct_assembly["method_details"].values[0],
+    "oligomeric_count": target_pdbx_struct_assembly["oligomeric_count"].values[0],
+    "oligomeric_details": target_pdbx_struct_assembly["oligomeric_details"].values[0],
+}
+# _pdbx_struct_assembly_gen
+pdbx_struct_assembly_gen = block.get_mmcif_category("_pdbx_struct_assembly_gen")
+df_pdbx_struct_assembly_gen = pd.DataFrame(pdbx_struct_assembly_gen)
+target_pdbx_struct_assembly_gen = df_pdbx_struct_assembly_gen.loc[
+    df_pdbx_struct_assembly_gen["assembly_id"] == "1"
+]
+pdbx_structure_assembly_gen_dict = {
+    "assembly_id": target_pdbx_struct_assembly_gen["assembly_id"].values[0],
+    "asym_id_list": target_pdbx_struct_assembly_gen["asym_id_list"].values[0],
+    "oper_expression": target_pdbx_struct_assembly_gen["oper_expression"].values[0],
+}
+# _pdbx_struct_oper_list
+pdbx_struct_oper_list = block.get_mmcif_category("_pdbx_struct_oper_list")
+# _refine.ls_d_res_high
+refine_ls_d_res_high = block.find_value("_refine.ls_d_res_high")
+# _software category addition
+software_dict = {
+    "classification": "other",
+    "name": "DeepMind Structure Class",
+    "pdbx_ordinal": "1",
+    "version": "2.0.0",
+}
+# _struct_asym
+struct_asym = block.get_mmcif_category("_struct_asym")
+df_struct_asym = pd.DataFrame(struct_asym)
+target_struct_asym = df_struct_asym.loc[df_struct_asym["id"] == target_chain]
+struct_asym_dict = {
+    "entity_id": target_struct_asym["entity_id"].values[0],
+    "id": target_struct_asym["id"].values[0],
+}
 # %%
-# %%
+outcif = f"/Users/YoshitakaM/Desktop/mmcif_files/{pdbid}_out.cif"
 newdoc = gemmi.cif.Document()
 newblock = newdoc.add_new_block(block.name)
 newblock.set_mmcif_category("_chem_comp", sorted_chem_comps)
@@ -123,7 +154,15 @@ newblock.set_pair(
     "_pdbx_database_status.recvd_initial_deposition_date", initial_deposition_date
 )
 newblock.set_mmcif_category("_pdbx_poly_seq_scheme", pdbx_polyseq_loop_dict)
+newblock.set_pairs("_pdbx_struct_assembly.", pdbx_struct_assembly_dict)
+newblock.set_pairs("_pdbx_struct_assembly_gen.", pdbx_structure_assembly_gen_dict)
+newblock.set_mmcif_category("_pdbx_struct_oper_list", pdbx_struct_oper_list)
+newblock.set_pair("_refine.ls_d_res_high", refine_ls_d_res_high)
+newblock.set_pairs("_software.", software_dict)
+newblock.set_pairs("_struct_asym.", struct_asym_dict)
 options = gemmi.cif.WriteOptions()
 options.misuse_hash = True
+options.align_loops = 20
+options.prefer_pairs = True
 newblock.write_file(outcif, options=options)
 # %%
